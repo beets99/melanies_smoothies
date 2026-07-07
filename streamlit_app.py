2330704ef0674e818e86259a903018080d126f53
beets99 <practicalkai@gmail.com>
2026-07-07 17:21:56 -0500

    Integrate Smoothiefroot API for nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-# Streamlit smoothie ordering app for Badge 3 DABW
-# Co-authored with CoCo
-# Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -33,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -44,8 +43,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-#new section to display smoothiefroot nutrition info
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
