a50f63dde3fb4d023a1b170ecc90562c13202e72
beets99 <practicalkai@gmail.com>
2026-07-09 10:47:26 -0500

    Remove st.stop() for continued data processing
    Removed st.stop() to allow further processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ st.write(
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
 
 # convert snowpark df to Pandas df to use LOC function
 pd_df=my_dataframe.to_pandas()
