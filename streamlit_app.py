5ab38c182db84d2c292fe1cd8e93d875d0f9bf99
beets99 <practicalkai@gmail.com>
2026-07-09 10:46:56 -0500

    Remove st.stop() to continue app execution
    Removed call to st.stop() to allow further execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ st.stop()
 # convert snowpark df to Pandas df to use LOC function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients"
