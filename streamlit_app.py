7a45161af19df091105427878df928e5873b6b9f
beets99 <practicalkai@gmail.com>
2026-07-09 10:14:08 -0500

    Add SEARCH_ON column to fruit options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write(
   """Choose the fruit you would like in your custom Smoothie!
   """)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
